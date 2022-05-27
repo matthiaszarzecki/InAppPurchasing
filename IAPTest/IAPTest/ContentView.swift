dc45f5a11f42f87dbaa487afa529b4008858ca66
Matthias Zarzecki <matthias.zarzecki@gmail.com>
2022-05-28 00:18:01 +0200

    Improve purchasing

--- IAPTest/IAPTest/ContentView.swift
+++ IAPTest/IAPTest/ContentView.swift
@@ -33,7 +33,15 @@ struct ContentView: View {
       )
 
       ForEach(models, id: \.self) { model in
-        Text(model.description)
+        Button(
+          action: {
+            productHandler.buyProduct(model)
+          },
+          label: {
+            Text("\(model.localizedTitle): \(model.localizedDescription), \(model.price)\(model.priceLocale.currencySymbol ?? "")")
+              .padding()
+          }
+        )
       }
     }
 
@@ -48,7 +56,83 @@ struct ContentView_Previews: PreviewProvider {
 
 public typealias ProductsRequestCompletionHandler = (_ success: Bool, _ products: [SKProduct]?) -> ()
 
-class ProductHandler: NSObject, SKProductsRequestDelegate {
+class ProductHandler: NSObject, SKProductsRequestDelegate, SKPaymentTransactionObserver {
+
+  override init() {
+    super.init()
+    SKPaymentQueue.default().add(self)
+  }
+
+  func paymentQueue(
+    _ queue: SKPaymentQueue,
+    updatedTransactions transactions: [SKPaymentTransaction]
+  ) {
+    for transaction in transactions {
+      switch transaction.transactionState {
+      case .purchased:
+        complete(transaction: transaction)
+        break
+      case .failed:
+        fail(transaction: transaction)
+        break
+      case .restored:
+        restore(transaction: transaction)
+        break
+      case .deferred:
+        break
+      case .purchasing:
+        break
+      @unknown default:
+        break
+      }
+    }
+  }
+
+  /// Finishes the purchasing process
+  private func complete(transaction: SKPaymentTransaction) {
+    print("completed purchase!")
+    deliverPurchaseNotificationFor(identifier: transaction.payment.productIdentifier)
+    SKPaymentQueue.default().finishTransaction(transaction)
+  }
+
+  /// Restores a previously purchased product
+  private func restore(transaction: SKPaymentTransaction) {
+    guard let productIdentifier = transaction.original?.payment.productIdentifier else {
+      return
+    }
+
+    print("restore... \(productIdentifier)")
+    deliverPurchaseNotificationFor(identifier: productIdentifier)
+    SKPaymentQueue.default().finishTransaction(transaction)
+  }
+
+  /// Fails the purchasing process
+  private func fail(transaction: SKPaymentTransaction) {
+    print("fail...")
+    if let transactionError = transaction.error as NSError? {
+      if transactionError.code != SKError.paymentCancelled.rawValue {
+        print("Transaction Error: \(String(describing: transaction.error?.localizedDescription))")
+      }
+    }
+
+    SKPaymentQueue.default().finishTransaction(transaction)
+  }
+
+  /// Posts a notification stating purchase or restoration was succesful
+  private func deliverPurchaseNotificationFor(identifier: String?) {
+    guard let identifier = identifier else {
+      return
+    }
+
+    //purchasedProductIdentifiers.insert(identifier)
+    UserDefaults.standard.set(true, forKey: identifier)
+    UserDefaults.standard.synchronize()
+    NotificationCenter.default.post(
+      name: NSNotification.Name(rawValue: "IAPHelperPurchaseNotification"),
+      object: identifier
+    )
+  }
+
   /// The function that returns the gotten products when fetching.
   /// Gets set when calling fetchProducts, and then actually called
   /// when products are returned.
@@ -84,5 +168,12 @@ class ProductHandler: NSObject, SKProductsRequestDelegate {
       print("Count: \(response.products.count)")
       self.completionHandler?(true, response.products)
     }
+  }
+
+  /// Starts the purchasing process for a product
+  func buyProduct(_ product: SKProduct) {
+    print("Buying \(product.productIdentifier)...")
+    let payment = SKPayment(product: product)
+    SKPaymentQueue.default().add(payment)
   }
 }
